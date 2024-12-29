99a0ffcdf4a2631340814bae54496acdff34495c
muhamsh <muhammad-ashraful@outlook.com>
2024-12-29 06:45:59 +0600

    fix db connection

--- queries/index.ts
+++ queries/index.ts
@@ -1,8 +1,11 @@
 import { hotelModel } from "@/models/hotels";
+import { dbConnect } from "@/services/mongoDB";
 import { replaceMongoIdInArray } from "@/utils/mongoData";
 
-export async function getAllHotels(): Promise<(Omit<IHotel, "_id"> & { id: string })[]> {
+export async function getAllHotels (): Promise<( Omit<IHotel, "_id"> & { id: string } )[]>
+{
+  await dbConnect();
   const hotels = await hotelModel.find().lean();
 
   return replaceMongoIdInArray(hotels);
-}
+};
