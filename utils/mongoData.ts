60d448ba1c0bd89209d2e2061f5e4b3216123735
muhamsh <muhammad-ashraful@outlook.com>
2024-12-29 23:24:15 +0600

    deployment types test

--- utils/mongoData.ts
+++ utils/mongoData.ts
@@ -1,11 +1,19 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-
 import { IHotel } from "@/models/hotels";
 
-export function replaceMongoIdInArray(hotels: any[]): IHotel[] {
-    return hotels.map( hotel => ( {
-        ...hotel,
-        id: hotel._id.toString(),
-        _id: undefined
-    } ) );
-}
+export function replaceMongoIdInArray ( hotels: any[] ): IHotel[]
+{
+
+    const data = hotels.map( hotel =>
+    {
+
+        const { _id, ...rest } = hotel;
+        return {
+            id: _id.toString(),
+            ...rest
+        } as IHotel;
+    } );
+
+    console.log( data );
+    return data;
+};
