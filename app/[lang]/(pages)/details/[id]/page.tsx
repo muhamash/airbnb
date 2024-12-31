43ca26f3802d1a4130d8e8cb322538c83b71d273
muhamsh <muhammad-ashraful@outlook.com>
2025-01-01 02:06:56 +0600

    dynamic meta data

--- app/[lang]/(pages)/details/[id]/page.tsx
+++ app/[lang]/(pages)/details/[id]/page.tsx
@@ -6,7 +6,24 @@ import { ObjectId } from "mongodb";
 import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 import { notFound } from "next/navigation";
 
-
+export async function generateMetadata({params}:Params) {
+  const hotel = await hotelModel.findOne( { _id: new ObjectId( params?.id ) } );
+  
+    return {
+        title: `Airbnb | Hotel | ${ hotel?.name }`,
+        description: hotel?.overview,
+        // openGraph: {
+        //   images:
+        //   {
+        //     url : `${process.env.NEXT_PUBLIC_API_URL}/og?title=${encodeURIComponent(
+        //       movieInfo?.movieDataById?.original_title
+        //     )}&description=${encodeURIComponent( movieInfo?.movieDataById?.overview )}&cover=${encodeURIComponent( `https://image.tmdb.org/t/p/original${movieInfo?.movieDataById?.poster_path}` )}`,
+        //     width: 1200,
+        //     height: 600,
+        //   }
+        // }
+    };
+};
 
 export default async function Details ( { params }: Params )
 {
