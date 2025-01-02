cf097eb19db819cc0e1615ca411b9476a16105f8
muhamsh <muhammad-ashraful@outlook.com>
2025-01-02 23:39:56 +0600

    payment form functioning

--- components/paymentDetails/PaymentForm.tsx
+++ components/paymentDetails/PaymentForm.tsx
@@ -1,30 +1,64 @@
-import Link from "next/link";
+// 'use client';
+
 import TripInfo from "./TripInfo";
 
+// import { useState } from 'react';
+
 export default async function PaymentForm() {
+
+    // const handleSubmit = async (e: React.FormEvent) => {
+    //     e.preventDefault();
+    //     setIsSubmitting(true);
+
+    //     try {
+    //         // Gather form data
+    //         const formData = new FormData(e.target as HTMLFormElement);
+    //         const data = Object.fromEntries(formData.entries());
+
+    //         console.log(data);
+    //     } catch (error) {
+    //         console.log(error);
+    //     } 
+    // };
+
     return (
-        <div>
-            <TripInfo/>
-            {/* <!-- Payment Section --> */}
+        <form>
+            <TripInfo />
+
+            {/* Payment Section */}
             <section className="mb-8">
                 <h2 className="text-xl font-semibold mb-4">
                     Pay with American Express
                 </h2>
                 <div className="space-y-4">
                     <input
-                        type="text"
+                        required
+                        type="tel"
+                        name="cardNumber"
                         placeholder="Card number"
                         className="w-full p-3 border rounded-lg"
+                        pattern="\d*" 
+                        inputMode="numeric"
+                        // onInput={( e: React.ChangeEvent<HTMLInputElement> ) =>
+                        // {
+                        //     e.target.value = e.target.value.replace( /\D/g, '' );
+                        // }}
                     />
 
                     <div className="grid grid-cols-2 gap-4">
                         <input
-                            type="text"
+                            required
+                            type="date"
+                            name="expiration"
                             placeholder="Expiration"
                             className="p-3 border rounded-lg"
                         />
                         <input
-                            type="text"
+                            required
+                            type="tel"
+                            name="cvv number"
+                            pattern="\d*" 
+                            inputMode="numeric"
                             placeholder="CVV"
                             className="p-3 border rounded-lg"
                         />
@@ -32,47 +66,57 @@ export default async function PaymentForm() {
                 </div>
             </section>
 
-            {/* <!-- Billing Address --> */}
+            {/* Billing Address */}
             <section className="mb-8">
                 <h2 className="text-xl font-semibold mb-4">Billing address</h2>
                 <div className="space-y-4">
                     <input
+                        required
                         type="text"
+                        name="streetAddress"
                         placeholder="Street address"
                         className="w-full p-3 border rounded-lg"
                     />
                     <input
+                        required
                         type="text"
+                        name="aptSuite"
                         placeholder="Apt or suite number"
                         className="w-full p-3 border rounded-lg"
                     />
                     <input
+                        required
                         type="text"
+                        name="city"
                         placeholder="City"
                         className="w-full p-3 border rounded-lg"
                     />
                     <div className="grid grid-cols-2 gap-4">
                         <input
+                            required
                             type="text"
+                            name="state"
                             placeholder="State"
                             className="p-3 border rounded-lg"
                         />
                         <input
+                            required
                             type="text"
+                            name="zipCode"
                             placeholder="ZIP code"
                             className="p-3 border rounded-lg"
                         />
                     </div>
                 </div>
             </section>
-
-            {/* <!-- Book Button --> */}
-            <Link
-                href="/success"
-                className="w-full block text-center bg-teal-500 text-white py-3 rounded-lg mt-6 hover:brightness-90"
+            {/* Submit Button */}
+            <button
+                type="submit"
+                // disabled={isSubmitting}
+                className={`w-full block text-center bg-teal-500 text-white py-3 rounded-lg mt-6 hover:brightness-90`}
             >
                 Request to book
-            </Link>
-        </div>
+            </button>
+        </form>
     );
 }
