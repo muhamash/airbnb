f02ad40fe51b9158ef423fbd3ee280ed90c55eed
muhamsh <muhammad-ashraful@outlook.com>
2025-01-17 05:30:50 +0600

    workiing on invoice -> pdf.....resolving headless and serverless pdf generation

--- app/api/download/invoice/route.tsx
+++ app/api/download/invoice/route.tsx
@@ -1,86 +1,95 @@
 import { fetchBookingDetails, fetchDictionary } from '@/utils/fetchFunction';
 import { generateHtml } from '@/utils/utils';
 import chromium from '@sparticuz/chromium-min';
-import puppeteer from 'puppeteer-core';
+import { NextResponse } from "next/server";
+import puppeteer, { type Browser } from 'puppeteer';
+import puppeteerCore, { type Browser as BrowserCore } from 'puppeteer-core';
 import QRCode from 'qrcode';
 
-export async function POST ( request: Request ): Promise<Response>
-{
-    try
-    {
-        const url = new URL( request.url );
-        const bookingId: string | null = url.searchParams.get( "bookingId" );
-        const hotelId: string | null = url.searchParams.get( "hotelId" );
-        const lang: string | null = url.searchParams.get( "lang" );
+export const dynamic = 'force-dynamic';
+export const maxDuration = 60;
 
-        if ( !bookingId || !hotelId || !lang )
-        {
-            return new Response( "Missing required parameters.", { status: 400 } );
+const isServer = process.env.IS_LOCAL ?? true;
+
+export async function POST(request: Request): Promise<Response> {
+    try {
+        const url = new URL(request.url);
+        const bookingId: string | null = url.searchParams.get("bookingId");
+        const hotelId: string | null = url.searchParams.get("hotelId");
+        const lang: string | null = url.searchParams.get("lang");
+
+        if (!bookingId || !hotelId || !lang) {
+            return new Response("Missing required parameters.", { status: 400 });
         }
 
-        const [ bookingDetails, language ] = await Promise.all( [
-            fetchBookingDetails( hotelId, bookingId ),
-            fetchDictionary( lang ),
+        const [bookingDetails, language] = await Promise.all([
+            fetchBookingDetails(hotelId, bookingId),
+            fetchDictionary(lang),
         ] );
         
         const qrCodeData = await QRCode.toDataURL(
-            `${ process.env.NEXT_PUBLIC_URL }/${ lang }/redirect?bookingId=${ bookingId }&hotelName=${ bookingDetails?.hotelName }&name=${ bookingDetails?.name }&hotelAddress=${ bookingDetails?.hotelAddress }&target=${ process.env.NEXT_PUBLIC_URL }/${ lang }/trip?bookingId=${ bookingId }&hotelId=${ hotelId }`
+            `${process.env.NEXT_PUBLIC_URL}/${lang}/redirect?bookingId=${bookingId}&hotelName=${bookingDetails?.hotelName}&name=${bookingDetails?.name}&hotelAddress=${bookingDetails?.hotelAddress}&target=${process.env.NEXT_PUBLIC_URL}/${lang}/trip?bookingId=${bookingId}&hotelId=${hotelId}`
         );
-        const content = await generateHtml( bookingDetails, language, qrCodeData );
+        const content = await generateHtml(bookingDetails, language, qrCodeData);
 
-        // chromium.setHeadlessMode = process.env.IS_LOCAL ? false : true;
+        let browser: Browser | BrowserCore;
         await chromium.font(
             "https://raw.githack.com/googlei18n/noto-emoji/master/fonts/NotoColorEmoji.ttf"
         );
 
-        const executablePath = await chromium.executablePath();
-        if ( !executablePath )
+        if ( isServer )
         {
-            throw new Error( "Chromium executable not found. Ensure @sparticuz/chromium-min is installed correctly." );
-        };
-
-        async function getBrowser ()
-        {
-            return puppeteer.launch( {
-                args: process.env.IS_LOCAL ? puppeteer.defaultArgs() : chromium.args,
-                defaultViewport: chromium.defaultViewport,
-                executablePath: process.env.IS_LOCAL
-                    ? "/tmp/localChromium/chromium/mac_arm-1407503/chrome-mac/Chromium.app/Contents/MacOS/Chromium"
-                    : await chromium.executablePath(),
-                headless: process.env.IS_LOCAL ? false : chromium.headless,
-            } );
+            const executablePath = await chromium.executablePath('https://github.com/Sparticuz/chromium/releases/download/v131.0.1/chromium-v131.0.1-pack.tar')
+            browser = await puppeteerCore.launch({
+                executablePath,
+                args: chromium.args,
+                headless: chromium.headless,
+                defaultViewport: chromium.defaultViewport
+            })
         }
-        // console.log( content );
-        const browser = await getBrowser();
+        else {
+            browser = await puppeteer.launch({
+                headless: true,
+                args: ['--no-sandbox', '--disable-setuid-sandbox']
+            })
+        }
+        
         const page = await browser.newPage();
-        await page.setContent( content, { waitUntil: "networkidle0" } );
-        
-        const pdfBuffer = await page.pdf( {
-            format: "A4",
+
+        await page.setContent(content, {
+            waitUntil: 'networkidle0'
+        });
+
+        const pdf = await page.pdf({
+            format: 'A4',
             printBackground: true,
-        } );
-        console.log( 'Browser launched' );
-        
+            margin: {
+                top: '10px',
+                right: '10px',
+                bottom: '10px',
+                left: '10px'
+            }
+        });
+
         await browser.close();
 
-        return new Response( pdfBuffer, {
+        return new NextResponse(pdf, {
+            status: 200,
             headers: {
                 'Content-Type': 'application/pdf',
-                'Content-Disposition': 'inline; filename="BookingConfirmation.pdf"',
+                'Content-Disposition': `attachment; filename=${resumeTitle}.pdf`,
             },
-            status: 200,
-        } );
+        });
 
-    } catch ( error )
-    {
-        console.error( 'Failed to generate PDF:', error );
+    } catch (error) {
+        console.error('Failed to generate PDF:', error);
         return new Response(
             JSON.stringify( {
                 status: 500,
                 success: false,
                 message: 'Failed to generate PDF.',
-            } ),
+            }),
             { status: 500, headers: { 'Content-Type': 'application/json' } }
         );
-    };
+    }
 }
--- next.config.mjs
+++ next.config.mjs
@@ -3,24 +3,12 @@ const nextConfig = {
     images: {
         domains: [ "lh3.googleusercontent.com", "a0.muscache.com", "images.unsplash.com", "a0.muscache.com", "example.com" ],
     },
-    webpack: ( config, { isServer } ) =>
-    {
-        config.module.rules.push( {
-            test: /\.map$/,
-            use: 'ignore-loader',
-        } );
-
-        if ( isServer )
-        {
-            config.externals = [
-                ...config.externals,
-                'puppeteer',
-                'chrome-aws-lambda',
-            ];
-        }
-
-        return config;
-    },
+    // experimental: {
+    //     serverComponentsExternalPackages: [
+    //         'puppeteer-core',
+    //         '@sparticuz/chromium'
+    //     ]
+    // },
     eslint: {
         ignoreDuringBuilds: true,
     },
