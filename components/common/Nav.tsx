2f7eea0d0c327529a13cbaad4689d147d1ad4904
muhamsh <muhammad-ashraful@outlook.com>
2025-01-19 02:53:05 +0600

    test..working on serach

--- components/common/Nav.tsx
+++ components/common/Nav.tsx
@@ -5,6 +5,7 @@ import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 import Image from "next/image";
 import Link from "next/link";
 import LanguageSwitch from "./LanguageSwitch";
+import Search from "./Search";
 import UserActions from "./UserActions";
 
 interface NavProps {
@@ -24,18 +25,7 @@ export default async function Nav ( { params }: NavProps )
                 </Link>
             </div>
 
-            <div className="row-start-2 col-span-2 border-[0.3px] border-slate-100 md:border flex shadow-md hover:shadow-sm transition-all md:rounded-full items-center px-2  ">
-                <div className="grid md:grid-cols-3 lg:grid-cols-7 gap-4 divide-x py-2 md:px-2 flex-grow">
-                    <input
-                        type="text"
-                        placeholder={responseData?.nav?.placeholder}
-                        className="px-3 bg-transparent focus:outline-none lg:col-span-3 text-violet-500 placeholder:text-sm"
-                    />
-                </div>
-                <button className="bg-cyan-600 w-9 h-9 rounded-full grid place-items-center text-sm text-center transition-all hover:brightness-90 shrink-0">
-                    <i className="fas fa-search text-slate-200" />
-                </button>
-            </div>
+            <Search placeholder={responseData?.nav?.placeholder} />
 
             <div className="flex items-center space-x-4 relative justify-end">
                 <LanguageSwitch />
--- components/common/Search.tsx
+++ components/common/Search.tsx
@@ -1,8 +1,23 @@
+'use client'
 
-export default function Search() {
+interface SearchProps
+{
+  placeholder: string;
+}
+
+export default function Search({placeholder}: SearchProps) {
   return (
-    <div>
-      
+    <div className="row-start-2 col-span-2 border-[0.3px] border-slate-100 md:border flex shadow-md hover:shadow-sm transition-all md:rounded-full items-center px-2  ">
+      <div className="grid md:grid-cols-3 lg:grid-cols-7 gap-4 divide-x py-2 md:px-2 flex-grow">
+        <input
+          type="text"
+          placeholder={placeholder}
+          className="px-3 bg-transparent focus:outline-none lg:col-span-3 text-violet-500 placeholder:text-sm"
+        />
+      </div>
+      <button className="bg-cyan-600 w-9 h-9 rounded-full grid place-items-center text-sm text-center transition-all hover:brightness-90 shrink-0">
+        <i className="fas fa-search text-slate-200" />
+      </button>
     </div>
-  )
+  );
 }
